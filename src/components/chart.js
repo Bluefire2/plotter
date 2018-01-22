c8e2739fb301187394b9d9d5ee37485a3a456a76
Bluefire2 <chernyshov.k@gmail.com>
2018-01-22 17:45:53 -0500

    Export parseVariable function into a separate file

--- src/components/chart.js
+++ src/components/chart.js
@@ -4,7 +4,7 @@ import * as math from 'mathjs';
 import * as _ from 'lodash';
 import {withFauxDOM} from 'react-faux-dom';
 
-import {Normal, Poisson, Binomial, NBinomial, Geometric} from '../distributions';
+import {parseVariable} from '../distributions';
 import variableColors from '../colors';
 
 /**
@@ -46,30 +46,6 @@ class Chart extends Component {
         //     d.frequency = +d.frequency;
         //     return d;
         // }
-
-        function parseVariable(text) {
-            const ref = {
-                "N": Normal,
-                "Po": Poisson,
-                "B": Binomial,
-                "NB": NBinomial,
-                "Geo": Geometric
-            };
-            let params, distribution;
-
-            try {
-                params = text.split("(")[1].split(")")[0].split(",").map(function (elem) {
-                    return parseFloat(elem);
-                }); // with readability in mind :)
-                distribution = text.split("(")[0];
-            } catch (e) {
-                // not a standard distribution
-                // assume it's a PDF
-                return text;
-            }
-            params.unshift(null);
-            return typeof ref[distribution] === "undefined" ? text : (new (Function.prototype.bind.apply(ref[distribution], params)));
-        }
 
         function variableIsDiscrete(variable) {
             // TODO: rewrite the entire distributions spec to make all variables extend a superclass
--- src/distributions.js
+++ src/distributions.js
@@ -349,3 +349,27 @@ export function NBinomial(r, p) {
 export function Geometric(p) { // a special case of the Negative Binomial distribution
     return new NBinomial(1, 1 - p);
 }
+
+export function parseVariable(text) {
+    const ref = {
+        "N": Normal,
+        "Po": Poisson,
+        "B": Binomial,
+        "NB": NBinomial,
+        "Geo": Geometric
+    };
+    let params, distribution;
+
+    try {
+        params = text.split("(")[1].split(")")[0].split(",").map(function (elem) {
+            return parseFloat(elem);
+        }); // with readability in mind :)
+        distribution = text.split("(")[0];
+    } catch (e) {
+        // not a standard distribution
+        // assume it's a PDF
+        return text;
+    }
+    params.unshift(null);
+    return typeof ref[distribution] === "undefined" ? text : (new (Function.prototype.bind.apply(ref[distribution], params)));
+}
