2e3a39c8b6dfb5f903a4abde8c48681c18d073ab
Bluefire2 <chernyshov.k@gmail.com>
2018-01-24 12:25:38 -0500

    Remove redundant constructor

--- src/containers/charts_container.js
+++ src/containers/charts_container.js
@@ -4,10 +4,6 @@ import {connect} from 'react-redux';
 import Chart from '../components/chart';
 
 class ChartsContainer extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         let chartElements;
         if(this.props.charts) {
