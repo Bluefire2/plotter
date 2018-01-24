e6dad157727ae04e343f0db092a8e9e6ebbd7d82
Bluefire2 <chernyshov.k@gmail.com>
2018-01-24 11:52:13 -0500

    Add another sample chart for testing with multiple charts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import reducers from './reducers';
 import './index.css';
@@ -10,19 +10,34 @@ import registerServiceWorker from './registerServiceWorker';
 
 const initialState = {
     // sample chart for testing:
-    charts: [{
-        variables: {
-            'X': 'B(10, 0.2)',
-            'Y': 'B(10, 0.5)',
-            'Z': 'B(10, 0.7)',
-            'H': 'N(3, 5)',
-            'I': 'N(6, 2)'
+    charts: [
+        {
+            variables: {
+                'X': 'B(10, 0.2)',
+                'Y': 'B(10, 0.5)',
+                'Z': 'B(10, 0.7)',
+                'H': 'N(3, 5)',
+                'I': 'N(6, 2)'
+            },
+            minX: 0,
+            maxX: 10,
+            minY: 0,
+            maxY: 0.4
         },
-        minX: 0,
-        maxX: 10,
-        minY: 0,
-        maxY: 0.4
-    }],
+        {
+            variables: {
+                'X': 'B(10, 0.2)',
+                'Y': 'B(10, 0.5)',
+                'Z': 'B(10, 0.7)',
+                'H': 'N(3, 5)',
+                'I': 'N(6, 2)'
+            },
+            minX: 0,
+            maxX: 10,
+            minY: 0,
+            maxY: 0.4
+        }
+    ],
     chartWidth: 1000,
     chartHeight: 500
 };
@@ -31,7 +46,7 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers, initialState)}>
-        <App />
+        <App/>
     </Provider>
     , document.getElementById('root'));
 
