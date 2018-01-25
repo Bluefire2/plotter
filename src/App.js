b1cca3b42a61314ffa4244bf11d93c149e7bfe04
Bluefire2 <chernyshov.k@gmail.com>
2018-01-25 08:35:07 -0500

    Finally remove that React template

--- src/App.js
+++ src/App.js
@@ -10,13 +10,12 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">Chart renderer</h1>
                 </header>
                 <div id="content">
                     <div id="sidebar">
                         <div className="sidebar-header">
-                            <h3>Add variables</h3>
+                            <h3>Add chart</h3>
                         </div>
                         <InputContainer/>
                     </div>
