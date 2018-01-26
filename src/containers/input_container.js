168428aec981170fd95f74a1531002dfe227d310
Bluefire2 <chernyshov.k@gmail.com>
2018-01-25 21:00:25 -0500

    Let the user set the range and domain of the chart

--- src/containers/input_container.js
+++ src/containers/input_container.js
@@ -7,15 +7,10 @@ class InputContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            title: '',
             variablesText: '',
-            title: ''
+            rangeText: ''
         };
-    }
-
-    textareaChange(e) {
-        this.setState({
-            variablesText: e.target.value
-        });
     }
 
     titleChange(e) {
@@ -24,37 +19,57 @@ class InputContainer extends Component {
         });
     }
 
+    variablesChange(e) {
+        this.setState({
+            variablesText: e.target.value
+        });
+    }
+
+    rangeChange(e) {
+        this.setState({
+            rangeText: e.target.value
+        });
+    }
+
     renderButtonClick() {
-        const text = this.state.variablesText,
-            lines = text.split('\n'),
-            variables = {},
-            title = this.state.title;
+        // TODO: write proper error handling if the user enters invalid input
+        const title = this.state.title,
+            variablesText = this.state.variablesText,
+            rangeText = this.state.rangeText;
 
-        const removeWhitespace = string => {
-            return string.replace(/ /g, '');
-        };
+        // helper methods
+        const removeWhitespace = string => string.replace(/ /g, ''),
+            stringToInt = string => +string;
 
-        lines.forEach(elem => {
+        // parse out variables
+        const variableLines = variablesText.split('\n'),
+            variables = {};
+        variableLines.forEach(elem => {
             const variableNameAndValue = elem.split('~').map(removeWhitespace),
                 name = variableNameAndValue[0];
 
             variables[name] = variableNameAndValue[1];
         });
 
+        // parse out range and domain
+        const rangeValues = rangeText.split(',').map(removeWhitespace).map(stringToInt),
+            [minX, maxX, minY, maxY] = rangeValues;
+
         this.props.addChart({
             title,
             variables,
-            minX: 0,
-            maxX: 10,
-            minY: 0,
-            maxY: 0.4
+            minX,
+            maxX,
+            minY,
+            maxY
         });
     }
 
     render() {
         const placeholders = {
+            title: 'My chart',
             variables: 'X~B(10, 0.3)',
-            title: 'My chart'
+            range: '0, 10, 0, 0.4'
         };
         return (
             <div id="input-container">
@@ -70,7 +85,14 @@ class InputContainer extends Component {
                     <textarea id="variable-input" className="form-control" placeholder={placeholders.variables}
                               rows="5"
                               value={this.state.variablesText}
-                              onChange={this.textareaChange.bind(this)}/>
+                              onChange={this.variablesChange.bind(this)}/>
+                </div>
+                <br/>
+                <div className="form-group">
+                    <label htmlFor="range-input">minX, maxX, minY, maxY:</label>
+                    <input id="range-input" className="form-control" type="text" placeholder={placeholders.range}
+                           value={this.state.range}
+                           onChange={this.rangeChange.bind(this)}/>
                 </div>
                 <br/>
                 <button className="btn" id="render-btn" onClick={this.renderButtonClick.bind(this)}>Render</button>
